d26cf6d8311719c54f4878e0666b8daee7589796
duuda2011@gmail.com <elissonalvessilva@MacBook-Pro-de-Elisson.local>
2022-10-19 19:14:56 -0300

    criado o layout do componente login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,63 @@
+import styled from "styled-components";
 import logo from "../assets/logo.png";
 export default function Login (){
   return(
-    <div className="container">
+    <EstiloContainer>
       <div className="logo">
         <img src={logo} alt="logo"></img></div>
         <div className="inputs">
           <input className="email" type="email" placeholder="email"></input>
           <input className="senha" type="password" placeholder="senha"></input>
         </div>
-        <bottom>Entrar</bottom>
+        <button>Entrar</button>
         <p>Não tem uma conta? Cadastre-se!</p>
-      </div>
-    
+    </EstiloContainer>
   )
 }
+
+ const EstiloContainer=styled.div`
+  padding: 40px 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: 'Lexend Deca', sans-serif;
+    .logo{
+      height: 178.38px;
+      width: 190px;
+      border-radius: 0px;
+      display: flex;
+      justify-content:center;
+      img {
+        width: 100%;
+      }
+    }
+    .inputs{
+      display: flex;
+      flex-direction: column;
+        input{
+          margin-bottom: 5px;
+          height: 45px;
+          width: 268px;
+          border: 1px solid #D5D5D5;
+          border-radius:4px ;
+          font-size: 18px;
+          padding: 6px;
+          color: #DBDBDB;
+
+        }
+    }
+    button{
+      padding: 11px 112px 11px 112px;
+      background-color: #52B6FF;
+      border: none;
+      border-radius: 4px;
+      color: #ffff;
+      font-size: 20px;
+      margin-bottom: 25px;
+    }
+    p{
+      font-size: 12px;
+      color:#52B6FF;
+    }
+
+ `
