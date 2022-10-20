a7ada6f97a97b26a89c67c83e02096e2e56b15e4
duuda2011@gmail.com <elissonalvessilva@MacBook-Pro-de-Elisson.local>
2022-10-20 15:39:28 -0300

    criado componente para circularprogressbar

---
+++ src/components/CircularProgressbar.js
@@ -0,0 +1,21 @@
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import "react-circular-progressbar/dist/styles.css";
+import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
+
+
+export default function CircularProgress(){
+  const value = 0.66;
+  return(
+    <CircularProgressbar value={value}
+          maxValue={1}
+          text={`${value * 100}%`}
+          background
+        backgroundPadding={6}
+        styles={buildStyles({
+          backgroundColor: "#52B6FF",
+          textColor: "#fff",
+          pathColor: "#fff",
+          trailColor: "transparent"
+        })}/>
+  )
+}
