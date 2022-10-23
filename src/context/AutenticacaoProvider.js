0c2e5d52328fd695a055bb0b2963590990045ac3
duuda2011@gmail.com <elissonalvessilva@MacBook-Pro-de-Elisson.local>
2022-10-23 18:46:10 -0300

    criado a funcao de verificar se existe o token

--- src/context/AutenticacaoProvider.js
+++ src/context/AutenticacaoProvider.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
+import { useNavigate } from "react-router-dom";
 
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 export const AutenticacaoContext = createContext([
   {},
@@ -9,9 +10,27 @@ export const AutenticacaoContext = createContext([
 
 export const AutenticacaoProvider = ({ children }) => {
   const [token, setToken] = useState("");
+  const navigate = useNavigate();
+
+  function armazenarToken(token) {
+    setToken(token);
+    localStorage.setItem('token', token);
+  }
+
+  useEffect(() => {
+    if(token === "") {
+      const tokenDoLocalStorage = localStorage.getItem("token");
+      if(!tokenDoLocalStorage) {
+        navigate("/");
+      }else {
+        setToken(tokenDoLocalStorage)
+      }
+    }
+    
+  }, [navigate, token])
 
   return (
-    <AutenticacaoContext.Provider value={[token, setToken]}>
+    <AutenticacaoContext.Provider value={[token, armazenarToken]}>
       {children}
     </AutenticacaoContext.Provider>
   )
