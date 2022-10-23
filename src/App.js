c5ee1b45e6cfda9c111df39a9470e4c2bce49520
duuda2011@gmail.com <elissonalvessilva@MacBook-Pro-de-Elisson.local>
2022-10-23 12:41:56 -0300

    adicionado o component de container

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
+import styled from "styled-components";
+
 import Login from "./components/Login"
 import Cadastrar from "./components/Cadastrar"
 import Navbar from "./components/Navbar"
 import Habitos from "./components/Habitos"
 import Footer from "./components/Footer"
-
+import Container from "./components/Container"
 export default function App(){
   return(
     /*<Login></Login>*/
     /*<Cadastrar></Cadastrar>*/
-    <>
-    <Navbar></Navbar>
-    <Habitos></Habitos>
-    <Footer></Footer>
-    </>
+    /*<Habitos></Habitos>*/
+    /*<Navbar></Navbar>*/
+    /*<Hoje></Hoje>*/
+    /*<Footer></Footer>*/
+    <EstiloApp>
+    
+    
+      <Navbar></Navbar>
+      <Container />
+    </EstiloApp>
     
   )
 }
+
+const EstiloApp = styled.div`
+  background-color: #F2f2f2;
+`
