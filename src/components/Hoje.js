342bc3be0ad452544b4a4e0cca2146a6b88c4750
duuda2011@gmail.com <elissonalvessilva@MacBook-Pro-de-Elisson.local>
2022-10-23 12:42:49 -0300

    criado component hoje

---
+++ src/components/Hoje.js
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+import MainCardHj from "./Main-cards-hj";
+
+
+export default function Hoje(){
+  return(
+    <>
+      <EstiloContainerHabitos>
+        <div className="top">
+          <h1>Segunda, 17/05</h1>
+          <div className="subtop">
+            <p className="porcentagem">67% dos hábitos concluídos</p> 
+            <p className="nadaconcluido">Nenhum hábito concluído ainda.</p> 
+          </div>
+        </div>
+      </EstiloContainerHabitos>
+
+      <MainCardsHoje>
+        <MainCardHj/>
+      </MainCardsHoje>
+    </>
+    
+  )
+}
+
+const EstiloContainerHabitos=styled.div`
+line-height: 24px;
+font-size:17px;
+  .top{
+    .subtop{
+      .porcentagem{
+        color:#8FC549;
+      }
+      .nadaconcluido{
+        color: #BABABA;
+      }
+    }
+
+    h1{
+      color: #126BA5;
+      font-size: 22px;
+      line-height: 28px;
+    }
+  }
+`
+
+const MainCardsHoje = styled.div`
+  margin-top: 28px;
+`
