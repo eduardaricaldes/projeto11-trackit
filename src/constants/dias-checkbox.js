c4414a01eef06be86aa982440f16825164c1c960
duuda2011@gmail.com <elissonalvessilva@MacBook-Pro-de-Elisson.local>
2022-10-23 15:12:32 -0300

    criado constante de habitos

---
+++ src/constants/dias-checkbox.js
@@ -0,0 +1,37 @@
+export const diasCheckbox = [
+    {
+      id: 0,
+      label: 'D',
+      value: 0
+    },
+    {
+      id: 1,
+      label: 'S',
+      value: 1
+    },
+    {
+      id: 2,
+      label: 'T',
+      value: 2
+    },
+    {
+      id: 3,
+      label: 'Q',
+      value: 3
+    },
+    {
+      id: 4,
+      label: 'Q',
+      value: 4
+    },
+    {
+      id: 5,
+      label: 'S',
+      value: 5
+    },
+    {
+      id: 6,
+      label: 'S',
+      value: 6
+    }
+  ]
